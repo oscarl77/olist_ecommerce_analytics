b3267a8e83bdaa6b82652f0f22998f152cbf4a08
oscarl77 <oscarlaurens18@gmail.com>
2026-08-04 20:15:31 +0100

    Built and tested order items staging model

---
+++ models/staging/stg__order_items.sql
@@ -0,0 +1,9 @@
+SELECT
+    CAST(order_id AS STRING) AS order_id,
+    CAST(order_item_id AS STRING) AS order_item_id,
+    CAST(product_id AS STRING) AS product_id,
+    CAST(seller_id AS STRING) AS seller_id,
+    SAFE_CAST(shipping_limit_date AS TIMESTAMP) AS shipping_limit_date,
+    SAFE_CAST(price AS NUMERIC) AS price,
+    SAFE_CAST(freight_value AS NUMERIC) AS freight_value
+FROM {{ source('raw_olist', 'order_items') }}
