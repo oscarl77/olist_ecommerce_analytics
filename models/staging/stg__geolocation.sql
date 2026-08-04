1ec48bdcf1758150a996a360b18afe5fe587ff57
oscarl77 <oscarlaurens18@gmail.com>
2026-08-04 20:05:18 +0100

    Built staging model for geolocation table

---
+++ models/staging/stg__geolocation.sql
@@ -0,0 +1,7 @@
+SELECT
+    CAST(geolocation_zip_code_prefix AS STRING) AS zip_code_prefix,
+    SAFE_CAST(geolocation_lat AS FLOAT64) AS geolocation_lat,
+    SAFE_CAST(geolocation_lng AS FLOAT64) AS geolocation_lng,
+    LOWER(TRIM(geolocation_city)) AS geolocation_city,
+    UPPER(TRIM(geolocation_state)) AS geolocation_state
+FROM {{ source('raw_olist', 'geolocation') }}
